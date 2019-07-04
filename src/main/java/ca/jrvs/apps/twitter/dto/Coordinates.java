f5f393156ef681c77b855a9ad0e23b1931886021
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-04 19:25:39 +0000

    Twitter DTO finished

--- src/main/java/ca/jrvs/apps/twitter/dto/Coordinates.java
+++ src/main/java/ca/jrvs/apps/twitter/dto/Coordinates.java
@@ -1,4 +1,22 @@
 package ca.jrvs.apps.twitter.dto;
 
 public class Coordinates {
+    private float[] coordinates;
+    private String type;
+
+    public float[] getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(float[] coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
--- src/main/java/ca/jrvs/apps/twitter/dto/Tweet.java
+++ src/main/java/ca/jrvs/apps/twitter/dto/Tweet.java
@@ -10,4 +10,87 @@ public class Tweet {
     private Entities entities;
     private Coordinates coordinates;
     private int retweet_count;
+    private int favorite_count;
+    private boolean favorited;
+    private boolean retweeted;
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getId_str() {
+        return id_str;
+    }
+
+    public void setId_str(String id_str) {
+        this.id_str = id_str;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Entities getEntities() {
+        return entities;
+    }
+
+    public void setEntities(Entities entities) {
+        this.entities = entities;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public int getRetweet_count() {
+        return retweet_count;
+    }
+
+    public void setRetweet_count(int retweet_count) {
+        this.retweet_count = retweet_count;
+    }
+
+    public int getFavorite_count() {
+        return favorite_count;
+    }
+
+    public void setFavorite_count(int favorite_count) {
+        this.favorite_count = favorite_count;
+    }
+
+    public boolean isFavorited() {
+        return favorited;
+    }
+
+    public void setFavorited(boolean favorited) {
+        this.favorited = favorited;
+    }
+
+    public boolean isRetweeted() {
+        return retweeted;
+    }
+
+    public void setRetweeted(boolean retweeted) {
+        this.retweeted = retweeted;
+    }
 }
