fb5bbaa8f0bf586aa97934d10fbc5efe99300348
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-08 18:34:39 +0000

    TwitterCLI added

--- src/main/java/ca/jrvs/apps/twitter/TwitterCLI.java
+++ src/main/java/ca/jrvs/apps/twitter/TwitterCLI.java
@@ -1,4 +1,19 @@
 package ca.jrvs.apps.twitter;
 
+import ca.jrvs.apps.twitter.dao.CrdRepository;
+import ca.jrvs.apps.twitter.dao.TwitterRestDao;
+import ca.jrvs.apps.twitter.dao.helper.ApacheHttpHelper;
+import ca.jrvs.apps.twitter.dao.helper.HttpHelper;
+import ca.jrvs.apps.twitter.service.TwitterService;
+import ca.jrvs.apps.twitter.service.TwitterServiceImp;
+
 public class TwitterCLI {
+    public static void main(String[] args) {
+        HttpHelper httpHelper = new ApacheHttpHelper();
+        CrdRepository crdRepository = new TwitterRestDao(httpHelper);
+        TwitterService twitterService = new TwitterServiceImp(crdRepository);
+        TwitterCLIRunner twitterCLIRunner = new TwitterCLIRunner(twitterService);
+
+        twitterCLIRunner.run(args);
+    }
 }
