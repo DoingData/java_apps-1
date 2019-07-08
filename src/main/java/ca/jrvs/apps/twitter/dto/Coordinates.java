755adcdc48875e553d25bcbfc482ab7b02974546
Miriam <miriam.eicherabe.jrvs.ca@gmail.com>
2019-07-08 18:20:58 +0000

    equals and hash added

--- src/main/java/ca/jrvs/apps/twitter/dto/Coordinates.java
+++ src/main/java/ca/jrvs/apps/twitter/dto/Coordinates.java
@@ -1,7 +1,10 @@
 package ca.jrvs.apps.twitter.dto;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Coordinates {
-    private double[] coordinates;
+    private double[] coordinates; //longitude, latitude
     private String type;
 
     public double[] getCoordinates() {
@@ -18,5 +21,20 @@ public class Coordinates {
 
     public void setType(String type) {
         this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return Arrays.equals(this.coordinates, that.coordinates) && this.type.equals(that.type);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(type);
+        result = 31 * result + Arrays.hashCode(coordinates);
+        return result;
     }
 }
